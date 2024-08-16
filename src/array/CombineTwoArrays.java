cb46cbb83b0ef387d760be8711dfcf4707e059ef
zzjiho <youngyoungnim@gmail.com>
2024-08-17 00:56:59 +0900

    두 배열 합치기

---
+++ src/array/CombineTwoArrays.java
@@ -0,0 +1,48 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// 두 배열 합치기
+public class CombineTwoArrays {
+    public ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int p1 = 0, p2 = 0; // 포인터
+        while (p1 < n && p2 < m) { // 둘 중 하나라도 끝까지 가면 종료
+            if (a[p1] < b[p2]) { // a배열의 값이 더 작으면
+                answer.add(a[p1++]); // a배열의 값을 추가하고 포인터 증가
+            } else {
+                answer.add(b[p2++]);
+            }
+        }
+        while (p1 < n) { // 남은 배열 추가 (둘 중 하나는 이미 끝까지 갔으므로)
+            answer.add(a[p1++]);
+        }
+        while (p2 < m) {
+            answer.add(b[p2++]);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        CombineTwoArrays T = new CombineTwoArrays();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = kb.nextInt();
+        }
+        int m = kb.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+            b[i] = kb.nextInt();
+        }
+        for (int x : T.solution(n, m, a, b)) {
+            System.out.print(x + " ");
+        }
+    }
+}
+// 3
+// 1 3 5
+// 5
+// 2 3 6 7 9
