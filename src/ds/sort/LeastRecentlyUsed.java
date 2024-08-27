4d4c53d784fe45bd68fd50489f99f16e1b0b8132
zzjiho <youngyoungnim@gmail.com>
2024-08-28 00:29:49 +0900

    LRU

--- src/ds/sort/LeastRecentlyUsed.java
+++ src/ds/sort/LeastRecentlyUsed.java
@@ -6,8 +6,6 @@ import java.util.Scanner;
  * Cache Miss : 23167 -> 52316
  * Cache Hit : 52316 -> 35216
  */
-//5 9
-//1 2 3 2 6 2 3 5 7
 public class LeastRecentlyUsed {
 
     public int[] solution(int size, int n, int[] arr) {
@@ -15,17 +13,16 @@ public class LeastRecentlyUsed {
         for (int x : arr) {
             int pos = -1;
             for (int i = 0; i < size; i++) {
-                if (x == cache[i]) {
-                    pos = i;
+                if (x == cache[i]) { // 캐시히트 (x가 캐시에 존재하면, 캐시에서 x를 제거하고, 맨 앞에 x를 삽입)
+                    pos = i; // 히트된 지점의 인덱스를 저장
                 }
             }
             if (pos == -1) { // 캐시미스 (캐시의 모든 항목을 오른쪽으로 한 칸씩 이동시키고, 캐시의 맨 앞에 x를 삽입)
                 for (int i = size - 1; i >= 1; i--) {
                     cache[i] = cache[i - 1];
                 }
-            }
-            else { // 캐시히트 (x의 위치 pos부터 맨 앞까지의 항목들을 오른쪽으로 한 칸씩 이동시키고, 캐시의 맨 앞에 x를 삽입)
-                for (int i = pos; i >= 1; i--) {
+            } else { // 캐시히트 (x의 위치 pos부터 맨 앞까지의 항목들을 오른쪽으로 한 칸씩 이동시키고, 캐시의 맨 앞에 x를 삽입)
+                for (int i = pos; i >= 1; i--) { // 히트난 지점부터 땡기기
                     cache[i] = cache[i - 1];
                 }
             }
@@ -42,10 +39,7 @@ public class LeastRecentlyUsed {
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
         for (int x : T.solution(s, n, arr)) System.out.print(x + " ");
-
     }
-
 }
-
-//5 9
-//1 2 3 2 6 2 3 5 7
+// 5 9
+// 1 2 3 2 6 2 3 5 7
